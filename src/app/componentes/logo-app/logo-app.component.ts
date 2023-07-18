288a48f3e58ee96d51b31e25cad16dc0cb0ebb6c
Yazvillalba <yazminvillalba55@gmail.com>
2023-07-18 18:54:58 -0300

    header

---
+++ src/app/componentes/logo-app/logo-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-app',
+  templateUrl: './logo-app.component.html',
+  styleUrls: ['./logo-app.component.css']
+})
+export class LogoAppComponent {
+
+}
